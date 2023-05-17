df2354a417dd4284c469a2c5661d72637bfebc59
Liuhd <783324487@qq.com>
2023-05-17 18:25:34 +0800

    finish1

--- application/models/mark.go
+++ application/models/mark.go
@@ -1,9 +1,9 @@
 package models
 
 type Mark struct {
-	ExamID    string `json:"exam_id" gorm:"not null" form:"exam_id"`
-	StudentID string `json:"student_id" gorm:"not null" form:"student_id"`
-	QuestionID string `json:"question_id" gorm:"not null" form:"question_id"`
-	Answer string `json:"answer" gorm:"not null" form:"answer"`
-	Score uint `json:"score" gorm:"not null" form:"score"`
+	ExamID     string `json:"exam_id" gorm:"not null;primary_key" form:"exam_id"`
+	StudentID  string `json:"student_id" gorm:"not null;primary_key" form:"student_id"`
+	QuestionID string `json:"question_id" gorm:"not null;primary_key" form:"question_id"`
+	Answer     string `json:"answer" form:"answer"`
+	Score      uint   `json:"score" form:"score"`
 }
---
+++ front/src/api/action/types.ts
@@ -0,0 +1,7 @@
+export interface Action {
+    action_id: string;
+    exam_id: string;
+    student_id: string;
+    answer: string;
+    action_time: number;
+}
